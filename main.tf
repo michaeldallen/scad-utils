512b598a117cfbbd4ad51a0fe1393844c96a14d8
michaeldallen <michael@allenhub.net>
2024-04-20 15:12:44 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.1"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.13.1"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+
+    http = {
+      source  = "hashicorp/http"
+      version = "3.4.2"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.5"
+    }
+  }
+}
